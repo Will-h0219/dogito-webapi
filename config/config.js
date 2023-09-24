26ee20c8a010345b1243ae0677e66f031ca28120
Will-h0219 <wherrera0219@gmail.com>
2023-09-23 19:16:52 -0500

    add development env

--- config/config.js
+++ config/config.js
@@ -2,7 +2,9 @@ require('dotenv').config();
 
 const config = {
   port: process.env.PORT || 4500,
-  connectionString: process.env.CONNECTION_STRING
+  connectionString: process.env.CONNECTION_STRING,
+  environment: process.env.NODE_ENV,
+  productionDomain: process.env.PROD_DOMAIN
 };
 
 module.exports = { config };
--- index.js
+++ index.js
@@ -5,8 +5,12 @@ const { dbConnection } = require('./config/database');
 const routerApi = require('./routing');
 
 const app = express();
+const corsOptions = {
+  origin: config.environment === 'DEV' ? 'http://localhost:3000/' : config.productionDomain,
+  optionsSuccessStatus: 200
+};
 
-app.use(cors());
+app.use(cors(corsOptions));
 
 app.use(express.json());
 
